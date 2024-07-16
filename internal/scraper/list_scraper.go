e82b2be051aef7c0efeff90c6c632e963a01f402
Aleksei Ivanov <aleksei.ivanov@utopiamusic.com>
2024-07-16 13:06:06 +0200

    Fetch first page using previous page link

--- internal/scraper/list_scraper.go
+++ internal/scraper/list_scraper.go
@@ -37,12 +37,21 @@ type parsePageCallback func(links []string, assets []string)
 // Run parses given page and paginates further.
 // Given callback is called with detailed links from each page.
 func (ls *ListScraper) Run(page string, cb parsePageCallback) error {
-	for {
-		links, assets, nextPage, err := ls.parsePage(page)
+	for i := 0; ; i++ {
+		links, assets, nextPage, prevPage, err := ls.parsePage(page)
 		if err != nil {
 			return err
 		}
 		cb(links, assets)
+
+		if i == 1 && prevPage != "" {
+			// need to fetch 1st page using "previous page" link because it's usually ../page-1.html and not ../index.html
+			prevPage = path.Join(path.Dir(page), prevPage)
+			_, _, _, _, err = ls.parsePage(prevPage)
+			if err != nil {
+				return err
+			}
+		}
 
 		if nextPage == "" {
 			break
@@ -85,33 +94,29 @@ func (ls *ListScraper) RunMultiple(pages []string, cb parsePageCallback) error {
 	return group.Wait()
 }
 
-func (ls *ListScraper) parsePage(pageURL string) ([]string, []string, string, error) {
+func (ls *ListScraper) parsePage(pageURL string) (links, images []string, nextPage, prevPage string, err error) {
 	ls.logger.Debug("parse list page", slog.String("url", pageURL))
 
 	resp, err := http.Get(absURL(pageURL))
 	if err != nil {
-		return nil, nil, "", fmt.Errorf("failed to get page: %w", err)
+		return nil, nil, "", "", fmt.Errorf("failed to get page: %w", err)
 	}
 	defer resp.Body.Close()
 
 	if resp.StatusCode != 200 {
-		return nil, nil, "", fmt.Errorf("unexpected response code: %d", resp.StatusCode)
+		return nil, nil, "", "", fmt.Errorf("unexpected response code: %d", resp.StatusCode)
 	}
 
 	page, err := saveObject(ls.dataDir, pageURL)
 	if err != nil {
-		return nil, nil, "", fmt.Errorf("failed to save page: %w", err)
+		return nil, nil, "", "", fmt.Errorf("failed to save page: %w", err)
 	}
 
 	doc, err := goquery.NewDocumentFromReader(bytes.NewReader(page))
 	if err != nil {
-		return nil, nil, "", fmt.Errorf("failed to build document: %w", err)
+		return nil, nil, "", "", fmt.Errorf("failed to build document: %w", err)
 	}
 
-	var (
-		links  []string
-		images []string
-	)
 	doc.Find("article.product_pod div.image_container a").Each(func(i int, s *goquery.Selection) {
 		href, _ := s.Attr("href")
 		img, _ := s.Find("img").Attr("src")
@@ -120,7 +125,8 @@ func (ls *ListScraper) parsePage(pageURL string) ([]string, []string, string, er
 		ls.logger.Debug("product href", slog.String("href", href), slog.String("img", img))
 	})
 
-	nextPage, _ := doc.Find("ul.pager li.next a").First().Attr("href")
-	ls.logger.Debug("parsed href with page", slog.String("next_page", nextPage))
-	return links, images, nextPage, nil
+	nextPage, _ = doc.Find("ul.pager li.next a").First().Attr("href")
+	prevPage, _ = doc.Find("ul.pager li.previous a").First().Attr("href")
+	ls.logger.Debug("parsed href with page", slog.String("next_page", nextPage), slog.String("prev_page", prevPage))
+	return links, images, nextPage, prevPage, nil
 }
